5cd923ca9c8b76491b1b5bad41b049b61056c32e
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2025-01-03 13:16:41 -0500

    Season Day six  episode four

--- JavaScript/Daysix.js
+++ JavaScript/Daysix.js
@@ -16,5 +16,32 @@ console.log((+a) + +"1")
 console.log(a * '1')
 console.log(a % '1')
 
+const fish = [
+    'Starfish',,
+     'Jellyfish',]
 
+console.log(fish)
+console.log(fish[1])
 
+const sales = 'Toyota';
+
+function carSales(name){
+    return name==='Honda'? name:`Sorry we don't sell ${name}`
+}
+
+const car = {
+    myCar:'Saturn',
+    getCar:carSales('Honda'),
+    special: sales
+}
+
+console.log(car.myCar)
+console.log(car.getCar)
+console.log(car.special)
+console.log(carSales('Jaguar'))
+
+const specialObj = {
+    obj:{a:'one',b:'two',c:'three'},
+    1:'viva'
+}
+console.log(specialObj.obj.a)
