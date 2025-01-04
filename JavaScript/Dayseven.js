0daadcdac285c4c068379f48f529b1f0a53cbbad
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2025-01-04 07:31:21 -0500

    Season Day seven  episode two

--- JavaScript/Dayseven.js
+++ JavaScript/Dayseven.js
@@ -6,3 +6,24 @@ if(condition === 'onee'){
 else {
     console.log(`Condition ${condition} is false.`)
 }
+
+let frutiType = 'Bananas';
+
+switch(frutiType){
+    case "Orange":
+        console.log("Orange are only $0.59 a pound")
+        break;
+    case "Apples":
+        console.log("Apple are only $0.32 a pound")
+        break;
+    case "Bananas":
+        console.log("Bananas are only $0.48 a pound")
+        break;
+    case "Cherries":
+        console.log("Cherries are only $3.00 a pound")
+        break;
+    default:
+        console.log(`Sorry, we are out of ${frutiType}`)
+    
+}
+console.log("Is there anything else you'd like?")
