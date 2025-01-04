bfa9ca647f27542a3bf2c6a154ff54cd11023eb7
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2025-01-04 08:09:52 -0500

    Season Day seven  episode five

--- JavaScript/Dayseven.js
+++ JavaScript/Dayseven.js
@@ -78,3 +78,26 @@ function f() {
   }
   console.log(f()); // 0, 1, 3, false
   
+  function f() {
+    try {
+      throw "bogus";
+    } catch (e) {
+      console.log('caught inner "bogus"');
+      // This throw statement is suspended until
+      // finally block has completed
+      throw e;
+    } finally {
+      return false; // overwrites the previous "throw"
+    }
+    // "return false" is executed now
+  }
+  
+  try {
+    console.log(f());
+  } catch (e) {
+    // this is never reached!
+    // while f() executes, the `finally` block returns false,
+    // which overwrites the `throw` inside the above `catch`
+    console.log('caught outer "bogus"');
+  }
+  
