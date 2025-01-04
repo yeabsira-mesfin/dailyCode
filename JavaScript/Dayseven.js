ee3c0cc71c7a66dd8a17ba2afe20e94ed48e0ff0
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2025-01-04 07:45:45 -0500

    Season Day seven  episode three

--- JavaScript/Dayseven.js
+++ JavaScript/Dayseven.js
@@ -1,29 +1,59 @@
-let condition = 'one';
+let condition = "one";
 
-if(condition === 'onee'){
-    console.log(`Condition ${condition} is true.`)
-}
-else {
-    console.log(`Condition ${condition} is false.`)
+if (condition === "onee") {
+  console.log(`Condition ${condition} is true.`);
+} else {
+  console.log(`Condition ${condition} is false.`);
 }
 
-let frutiType = 'Bananas';
+let frutiType = "Bananas";
 
-switch(frutiType){
-    case "Orange":
-        console.log("Orange are only $0.59 a pound")
-        break;
-    case "Apples":
-        console.log("Apple are only $0.32 a pound")
-        break;
-    case "Bananas":
-        console.log("Bananas are only $0.48 a pound")
-        break;
-    case "Cherries":
-        console.log("Cherries are only $3.00 a pound")
-        break;
-    default:
-        console.log(`Sorry, we are out of ${frutiType}`)
-    
+switch (frutiType) {
+  case "Orange":
+    console.log("Orange are only $0.59 a pound");
+    break;
+  case "Apples":
+    console.log("Apple are only $0.32 a pound");
+    break;
+  case "Bananas":
+    console.log("Bananas are only $0.48 a pound");
+    break;
+  case "Cherries":
+    console.log("Cherries are only $3.00 a pound");
+    break;
+  default:
+    console.log(`Sorry, we are out of ${frutiType}`);
 }
-console.log("Is there anything else you'd like?")
+console.log("Is there anything else you'd like?");
+
+function getMonthName(mo) {
+  mo--;
+
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  if(months[mo]){
+    return months[mo];
+  } else {
+    throw new Error("InvalidMonthNo");
+  }
+}
+console.log(getMonthName(11)) 
+try {
+    monthName = getMonthName(myMonth);
+} catch(e){
+    monthName = "Unknown";
+    // logMyErrors(e);
+}
